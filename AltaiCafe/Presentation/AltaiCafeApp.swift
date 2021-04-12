9f06e5545a7be92befceddc7c951720b522b1efa
Roman Baev <baevra@gmail.com>
2021-04-12 12:08:30 +0300

    Update folders structure

---
+++ AltaiCafe/Presentation/AltaiCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltaiCafeApp.swift
+//  AltaiCafe
+//
+//  Created by Roman Baev on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AltaiCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Hello")
+        }
+    }
+}
